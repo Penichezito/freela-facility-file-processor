3bc2143ce894ed3cc59191859ca87f7ecc4e96ab
Penichezito <penichebarbosa88@gmail.com>
2025-04-06 23:49:47 -0300

    "Implatação database.py"

---
+++ app/db/database.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(metada=metadata)
+
+def init_db(app):
+    """Inicializa o banco de dados com a aplicação Flask"""
+    db.init_app(app)
+
+    with app.app_context():
+        # Importe todos os modelos aqui para garantir que eles sejam registrados com o SQLAlchemy
+        from app.db.models.file import File
+        from app.db.models.tag import Tag
+
+        # Crie todas as tabelas no banco de dados
+        db.create_all()
